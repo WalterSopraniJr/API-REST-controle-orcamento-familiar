161abb445cb5b6c5c2af3f4eb8e4582fb3a24460
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-24 06:17:17 -0300

    Permite adicionar mais clientes pelo servidor.

--- index.js
+++ index.js
@@ -5,11 +5,34 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+let db = [
+    {'1': {cliente: 'Walter Soprani', idade: '26'}},
+    {'2': {cliente: 'Beatriz Oliveira', idade: '24'}},
+    {'3': {cliente: 'Jorge Mingatos', idade: '1'}}
+];
+
+//Busca Dados
+app.get('/', (request, response) => {
+    return response.json(db);
+})
+
+//Insere Dados
+app.post('/add', (request, response) => {
+    const body = request.body;
+
+    if (!body) 
+        return response.status(400).end()
+    
+
+    db.push(body)
+    return response.json(body)
+})
 
 app.listen(3000, () => {
     console.log('Express started at http://localhost:3000')
-})
+});
