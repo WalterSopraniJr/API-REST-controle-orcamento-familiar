1cacc0ddc3a7c338c69c0a7f294aa6a2039de96b
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-24 22:18:57 -0300

    Pasta rotas criada, função de deletar implementada.

---
+++ config/routes.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const routes = express.Router()
+
+
+
+let db = [
+    {'1': {cliente: 'Walter Soprani', idade: '26'}},
+    {'2': {cliente: 'Beatriz Oliveira', idade: '24'}},
+    {'3': {cliente: 'Jorge Mingatos', idade: '1'}}
+];
+
+//Busca Dados
+routes.get('/', (request, response) => {
+    return response.json(db);
+})
+
+//Insere Dados
+routes.post('/add', (request, response) => {
+    const body = request.body;
+
+    if (!body) 
+        return response.status(400).end();
+    
+
+    db.push(body);
+    return response.json(body);
+})
+
+routes.delete('/:id', (request, response) => {
+    const id = request.params.id
+
+    let newDB = db.filter(item => {
+        if(!item[id])
+            return item
+    })
+
+    db = newDB
+    
+    return response.send(newDB)
+})
+
+module.exports = routes
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const routes = require('./config/routes')
 
 const app = express();
 
@@ -9,30 +10,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
+app.use(routes)
 
-let db = [
-    {'1': {cliente: 'Walter Soprani', idade: '26'}},
-    {'2': {cliente: 'Beatriz Oliveira', idade: '24'}},
-    {'3': {cliente: 'Jorge Mingatos', idade: '1'}}
-];
-
-//Busca Dados
-app.get('/', (request, response) => {
-    return response.json(db);
-})
-
-//Insere Dados
-app.post('/add', (request, response) => {
-    const body = request.body;
-
-    if (!body) 
-        return response.status(400).end()
-    
-
-    db.push(body)
-    return response.json(body)
-})
 
 app.listen(3000, () => {
     console.log('Express started at http://localhost:3000')
 });
+
